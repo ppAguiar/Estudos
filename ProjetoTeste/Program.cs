bd59b3332b85abb28d5e21986472a146667ac01d
ppaguiar.silva@gmail.com <ppaguiar.silva@gmail.com>
2020-11-29 15:32:20 -0300

    Nova insercao

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -9,6 +9,7 @@ namespace ProjetoTeste
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("");
             Console.WriteLine("Como vc está?");
+            Console.WriteLine("Novo teste");
         }
     }
 }
