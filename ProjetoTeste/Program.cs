d7fb5fcfb0c6de867dd93a1519da751a0a887621
ppAguiar <ppaguiar.silva@gmail.com>
2020-11-29 22:52:17 -0300

    Entrada de dados Funcionario/impressao de dados

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using ProjetoTeste.Entities;
+using ProjetoTeste.Entities.Enuns;
+using System.Globalization;
 
 namespace ProjetoTeste
 {
@@ -7,10 +9,33 @@ namespace ProjetoTeste
     {
         static void Main(string[] args)
         {
+            /*-------------------------------------
+             *  Dados Departamento
+             --------------------------------------*/
             Console.Write("Enter department's name: ");
             Departament dep = new Departament(Console.ReadLine());
+            /*-------------------------------------
+             *  Dados Trabalhador
+             --------------------------------------*/
 
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel wl;
+            Enum.TryParse(Console.ReadLine(), out wl);
+            Console.Write("Base salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Worker worker = new Worker(name, wl, salary);
+
+            /*-------------------------------------
+             *  Impressão de dados
+             --------------------------------------*/
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Name: " + worker.Name);
             Console.WriteLine(dep);
+
 
         }
     }
