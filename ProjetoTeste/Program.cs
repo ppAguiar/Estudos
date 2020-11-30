471f6722c8ad111e2fd522b61fb9fa43bb3ebaf3
ppAguiar <ppaguiar.silva@gmail.com>
2020-11-29 23:03:20 -0300

    Inserido Contratos

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -27,6 +27,24 @@ namespace ProjetoTeste
             Console.Write("Base salary: ");
             double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Worker worker = new Worker(name, wl, salary);
+            /*-------------------------------------
+             *  Entrada de Contratos
+             --------------------------------------*/
+            Console.Write("How many contracts to this worker? ");
+            int numeroDeContratos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numeroDeContratos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter #{0} contract data:", i);
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int hour = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, value, hour);
+            }
 
             /*-------------------------------------
              *  Impressão de dados
@@ -35,8 +53,6 @@ namespace ProjetoTeste
             Console.WriteLine("------------------------------");
             Console.WriteLine("Name: " + worker.Name);
             Console.WriteLine(dep);
-
-
         }
     }
 }
