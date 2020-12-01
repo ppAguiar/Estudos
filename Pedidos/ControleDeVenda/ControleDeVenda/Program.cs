ad1d706f75386547a78be890c26e6ebd793b1db3
ppAguiar <ppaguiar.silva@gmail.com>
2020-11-30 21:39:55 -0300

    Criado novo Projeto  Controle de Pedidos

---
+++ Pedidos/ControleDeVenda/ControleDeVenda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControleDeVenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
